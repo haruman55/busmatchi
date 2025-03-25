19f77b0ef11716e4ba3dd266b3b4b12683a5ff66
tanaka-tai <77325265+tanaka-tai@users.noreply.github.com>
2025-03-25 06:03:11 +0000

    vuetifyテーマ追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   ssr: false,
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module",'@nuxt/eslint'],
-
+  modules: ['vuetify-nuxt-module', '@nuxt/eslint'],
+  vuetify: {
+    vuetifyOptions: {
+      theme: {
+        themes: {
+          light: {
+            colors: {
+              primary: '#05677e',
+              secondary: '#a3bbc5',
+              background: '#f8f3ed',
+            },
+          },
+        },
+      },
+    },
+  },
 })
