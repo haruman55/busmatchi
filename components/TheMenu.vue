35f34fc03a995ef7714ef5164bae426929dc5856
haruman55 <harukayoshihara@gmail.com>
2025-04-05 22:00:37 +0900

    companyIdの表示を削除。ユーザが意識するIDではないので

--- components/TheMenu.vue
+++ components/TheMenu.vue
@@ -1,16 +1,26 @@
 <template>
   <v-toolbar density="compact" color="indigo-lighten-3">
 
-    <h3> {{ userInfo.companyName }}さん({{ userInfo.companyId }})</h3>
+    <h3 class="ml-10"> {{ userInfo.companyName }} </h3>
     <v-spacer />
 
     <v-menu v-model="menu" open-on-hover location="bottom" offset="0">
       <template #activator="{ props }">
         <v-btn icon="mdi-dots-grid" size="x-large" v-bind="props"></v-btn>
       </template>
-      <v-sheet width="300" height="200" class="pa-2" color="white">
-        <v-btn size="x-large" icon="mdi-home" variant="text" @click="navigateTo('/user/mypage')"></v-btn>
-        <v-btn v-if="auth === 1" size="x-large" icon="mdi-account-plus-outline" variant="text" @click="navigateTo('/manager/member/list')"></v-btn>
+      <v-sheet width="300" height="150" class="pa-2" color="white" style="overflow-x: hidden;"> 
+        <v-row
+justify="center"
+           class="text-center">
+          <v-col cols="auto">
+            <v-icon icon="mdi-home" size="36" @click="navigateTo('/user/mypage')" />
+            <div>マイページ</div>
+          </v-col>
+          <v-col v-if="auth === 1" cols="auto">
+            <v-icon icon="mdi-account-plus-outline" size="36" @click="navigateTo('/manager/member/list')" />
+            <div>利用者管理</div>
+          </v-col>
+        </v-row>
       </v-sheet>
     </v-menu>
 
@@ -31,7 +41,7 @@ const menu = ref(false);
  */
 const navigateTo = (path) => {
   menu.value = false; // メニューを閉じる
-  router.push(path);  
+  router.push(path);
 };
 
 
